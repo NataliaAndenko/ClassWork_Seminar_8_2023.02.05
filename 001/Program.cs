6635b6ac93037e74e7f36285fb792a28eddc93fe
Natalia <zhuravlevanyu@mail.ru>
2023-02-05 16:00:06 +0300

    qq

---
+++ 001/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 1: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+*/
+
+int[,] matrix = new int[5, 5];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10)
+    ;
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+    }
+}
+
+void Change(int[,] arr)
+{
+    var temp = 0;
+    int lastNum = arr.GetLength(0) - 1;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        temp = arr[0, i];
+        arr[0, i] = arr[lastNum, i];
+        arr[lastNum, i] = temp;
+    }
+}
+//int[,] matrix = new int[5, 5];
+FillArray(matrix);
+PrintArray(matrix);
+System.Console.WriteLine();
+Change(matrix);
+PrintArray(matrix);
---
+++ 002/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 2: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+*/
+
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void Change(int[,] arr)
+{
+    var temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = i; j < arr.GetLength(1); j++)
+        {
+            temp = arr[i, j];
+            arr[i, j] = arr[j, i];
+            arr[j, i] = temp;
+        }
+    }
+}
+int[,] massive = new int[5, 5];
+FillArray(massive);
+PrintArray(massive);
+System.Console.WriteLine();
+Change(massive);
+PrintArray(massive);
